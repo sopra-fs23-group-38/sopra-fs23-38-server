645afc1180952e72c4abcc4fad60b7385a0f7b90
Qiyue Shang <91901609+QiyueShang666@users.noreply.github.com>
2023-05-24 16:24:16 +0200

    Update CommentServiceTest.java

--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/CommentServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/CommentServiceTest.java
@@ -6,11 +6,13 @@ import ch.uzh.ifi.hase.soprafs23.entity.Notification;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
 import ch.uzh.ifi.hase.soprafs23.repository.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -19,6 +21,8 @@ import java.math.BigDecimal;
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -35,6 +39,11 @@ class CommentServiceTest {
     private AnswerRepository mockAnswerRepository;
     @Mock
     private NotificationRepository mockNotificationRepository;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @InjectMocks
     private CommentService commentServiceUnderTest;
@@ -210,7 +219,7 @@ class CommentServiceTest {
         final String result = commentServiceUnderTest.getAllComments(0);
 
         // Verify the results
-        Assertions.assertTrue(result == null || result.isEmpty() || result.equals("[]"));
+        assertTrue(result == null || result.isEmpty() || result.equals("[]"));
     }
 
     @Test
@@ -244,7 +253,7 @@ class CommentServiceTest {
         final String result = commentServiceUnderTest.getAllComments(0);
 
         // Verify the results
-        Assertions.assertTrue(result == null || result.isEmpty() || result.equals("[]"));
+        assertTrue(result == null || result.isEmpty() || result.equals("[]"));
     }
 
     @Test
@@ -277,7 +286,7 @@ class CommentServiceTest {
         final String result = commentServiceUnderTest.getAllComments(0);
 
         // Verify the results
-        Assertions.assertTrue(result == null || result.isEmpty() || result.equals("[]"));
+        assertTrue(result == null || result.isEmpty() || result.equals("[]"));
     }
     @Test
     void testGetCommentsByWho() {
@@ -373,32 +382,73 @@ class CommentServiceTest {
     }
 
     @Test
-    void addRepliesToList() {
-        // Setup
-        final Comment comment1 = new Comment();
-        comment1.setId(1);
-        comment1.setContent("content");
-        comment1.setWho_comments(0);
-        comment1.setAnswer_ID(0);
-        comment1.setChange_time(new GregorianCalendar(2020, Calendar.JANUARY, 1).getTime());
-        comment1.setParentCommentId(0);
-        final List<Comment> comments1 = List.of(comment1);
-        when(mockCommentRepository.findByParentID(Mockito.any())).thenReturn(comments1);
-        final User user1 = new User();
-        user1.setId(0);
-        user1.setUsername("username");
-        user1.setPassword("password");
-        user1.setEmail("email");
-        user1.setHasNew(0);
-        final Optional<User> user = Optional.of(user1);
-        when(mockUserRepository.findById(Mockito.any())).thenReturn(user);
-        List<Map<String, Object>> repliesList  = new ArrayList<>();
-        // Run the test
-        commentServiceUnderTest.addRepliesToList(repliesList,comment1);
+    void testAddRepliesToList() {
+        // Given
+        Comment comment = new Comment();
+        comment.setId(1);
+        comment.setWho_comments(2);
+        comment.setChange_time(new Date());
+        comment.setContent("Test Comment");
 
-        // Verify the results
-        //Assertions.assertNotNull(result);
+        Comment reply = new Comment();
+        reply.setId(3);
+        reply.setWho_comments(4);
+        reply.setChange_time(new Date());
+        reply.setContent("Reply Comment");
+
+        User user = new User();
+        user.setUsername("Test User");
+        user.setAvatarUrl("testAvatarUrl");
+
+        when(mockCommentRepository.findByParentID(1)).thenReturn(Arrays.asList(reply));
+        when(mockUserRepository.findById(4)).thenReturn(Optional.of(user));
+
+        List<Map<String, Object>> repliesList = new ArrayList<>();
+
+        // When
+        commentServiceUnderTest.addRepliesToList(repliesList, comment);
+
+        // Then
+        assertEquals(1, repliesList.size());
+        Map<String, Object> replyMap = repliesList.get(0);
+        assertEquals(user.getUsername(), replyMap.get("author"));
+        assertEquals(user.getAvatarUrl(), replyMap.get("author_avatar"));
+        assertEquals(reply.getId(), replyMap.get("id"));
+        assertEquals(reply.getChange_time(), replyMap.get("time"));
+        assertEquals(reply.getContent(), replyMap.get("content"));
+        assertEquals(0, ((List)replyMap.get("replies")).size()); // Checking for no replies of reply
     }
+
+//    @Test
+//    void addRepliesToList() {
+//        // Setup
+//        final Comment comment1 = new Comment();
+//        comment1.setId(1);
+//        comment1.setContent("content");
+//        comment1.setWho_comments(0);
+//        comment1.setAnswer_ID(0);
+//        comment1.setChange_time(new GregorianCalendar(2020, Calendar.JANUARY, 1).getTime());
+//        comment1.setParentCommentId(0);
+//        final List<Comment> comments1 = List.of(comment1);
+//        when(mockCommentRepository.findByParentID(Mockito.any())).thenReturn(comments1);
+//        final User user1 = new User();
+//        user1.setId(0);
+//        user1.setUsername("username");
+//        user1.setPassword("password");
+//        user1.setEmail("email");
+//        user1.setHasNew(0);
+//        final Optional<User> user = Optional.of(user1);
+//        when(mockUserRepository.findById(Mockito.any())).thenReturn(user);
+//        List<Map<String, Object>> repliesList  = new ArrayList<>();
+//        // Run the test
+//        commentServiceUnderTest.addRepliesToList(repliesList,comment1);
+//
+//        // Verify the results
+//        //Assertions.assertNotNull(result);
+//    }
+
+
+
 
     @InjectMocks
     SearchService searchService;
